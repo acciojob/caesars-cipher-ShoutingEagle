fe1d4fe7f1e927f7f1b05b464905c9a476b5b393
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-07-04 04:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,21 +34,13 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
- for(let i=0;i<encidedStr;i++){
-	 let  temp = encodedStr[0];
-	 decodedArr.push(lookTable(temp));
-	 
+ for(let i=0;i<encodedStr.length;i++){
+	 decodedArr.push(lookup[encodedStr[i]]);
  }
   return decodedArr; //return decodedArr
 }
 
-function lookTable(parameter){
-	for (let x in lookup){
-		if(x === parameter){
-			return lookup[x];
-		}
-	}
-}
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
